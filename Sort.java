82de9032dc912129149d2d303d0f335597d883d0
chloehammes <47236158+chloehammes@users.noreply.github.com>
2019-03-07 08:14:47 -0600

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -7,6 +7,17 @@ class Main
     void sort(double arr[], long start, long end) 
     { 
       // sort the array here
+      int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+          for (int j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1])
+        {
+          //swapping
+          double temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+     
     } 
 
   
